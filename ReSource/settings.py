3eca329126d59219ed39f95d15f53484e8190aec
mathena828 <mathena828@gmail.com>
2020-01-19 00:57:43 +0800

    Remove Sites

--- ReSource/settings.py
+++ ReSource/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'crispy_forms',
     'phonenumber_field',
-    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,8 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
-SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
